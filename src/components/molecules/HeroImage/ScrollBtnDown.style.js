9c45203f9aa28c4581533e41d3f876e3ec424eac
roguelikedev <agdgroguelikedev@gmail.com>
2018-08-04 14:27:25 -0700

    float scrolldown btn

--- src/components/molecules/HeroImage/ScrollBtnDown.style.js
+++ src/components/molecules/HeroImage/ScrollBtnDown.style.js
@@ -1,14 +1,27 @@
 // @flow
 
 // node_modules
-import { css } from "styled-components";
+import { css, keyframes } from "styled-components";
 import { prop } from "styled-tools";
 
 // Constants
 import { ThemeProps } from "Base/MainTheme";
 import { Depths, Measurements } from "Base/Constants";
 
+const float = keyframes`
+    0% {
+		transform: translatey(0px);
+	}
+    50% {
+		transform: translatey(20px);
+    }
+	100% {
+		transform: translatey(0px);
+	}
+`;
+
 export default css`
+animation: ${float} 2s ease-in-out infinite
     cursor: pointer;
     color: ${prop(ThemeProps.white)};
     z-index: ${Depths.high};
